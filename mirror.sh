4f4a95ee3ddcaf0103495a32920fdb8efbd29346
John Wass <wassj@ctc.com>
2017-08-16 17:28:34 -0400

    detecting changed branches

--- mirror.sh
+++ mirror.sh
@@ -15,6 +15,23 @@ list_branches() {
   git branch -v --no-abbrev | sed -e 's/*//g' | awk '{print $1"="$2}'
 }
 
+list_changed_branches() {
+  cd "$tmpdir"
+  source "$history"
+
+  for b in $(list_branches); do
+    IFS='=' read -ra B <<< "$b"
+    compare_branch "${B[@]}"
+  done
+}
+
+compare_branch() {
+  local name="$1"
+  local hash="$2"
+
+  if [[ "$hash" != "${!name}" ]]; then echo "$name"; fi
+}
+
 clone() {
   local source="$1"
 
@@ -45,9 +62,11 @@ mirror() {
   clone "$source"
   push --mirror "$target"
 
-  add_file https://raw.githubusercontent.com/jw3/openshift-kinesalite/master/template.yml 'a file from another repo'
-  push "$target"
+  for b in $(list_changed_branches); do
+    git checkout "$b"
+    add_file https://raw.githubusercontent.com/jw3/example-mirrored-cicfg/master/.gitlab-ci.yml 'ci config'
+    push "$target"
+  done
 }
 
 "$@"
-
